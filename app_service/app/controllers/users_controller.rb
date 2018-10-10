3f45b24124e85d4168c2ed1b6711d8b14e398c48
Zoid <brandincurry@gmail.com>
2018-10-10 17:34:11 -0400

    proile date display update, some css, some commenting, etc.

--- app_service/app/controllers/users_controller.rb
+++ app_service/app/controllers/users_controller.rb
@@ -10,8 +10,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      #log the user into the site on creation
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the pngCrypt!"
+      #the following line redirects to the user profile
+      #redirect_to @user
+      #the following line redirects to the application page
       redirect_to stenApp_path
     else
       render 'new'
@@ -19,7 +23,6 @@ class UsersController < ApplicationController
   end
 
   private
-
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
