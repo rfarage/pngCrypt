0ac1a2bf540e53bcc48e267e58b51965c858245e
Zoid <brandincurry@gmail.com>
2018-10-10 15:07:50 -0400

    i think this fixed the user and started building sessions

--- app_service/db/schema.rb
+++ app_service/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_185300) do
+ActiveRecord::Schema.define(version: 2018_10_03_215455) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,8 @@ ActiveRecord::Schema.define(version: 2018_10_03_185300) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.integer "ImageUploads", null: false, default: 0
+    t.integer "ImageUploads", default: 0
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
