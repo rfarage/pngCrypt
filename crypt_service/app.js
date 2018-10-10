acaa9b7c43a7484f1cd5063866e4478fe2e3dfc5
michael yee <michael.yee@triblio.com>
2018-10-10 13:30:42 -0400

    crypt service

--- crypt_service/app.js
+++ crypt_service/app.js
@@ -5,13 +5,10 @@ const bodyParser = require('body-parser');
 const fs = require("fs");
 const steggy = require('steggy')
 
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 // parse application/json
 app.use(bodyParser.json({limit: '50mb', extended: true}))
-
-
 
 //PING 
 app.get('/ping', function (req, res) {
@@ -22,15 +19,10 @@ app.get('/ping', function (req, res) {
 app.post('/ping', function (req, res) {
   var base64Data = req.body.imageData.replace(/^data:image\/png;base64,/, "");
   var original = new Buffer(base64Data, 'base64');
-  var message = "HelloDarkness";
+  var message = req.body.message;
 
   var buffer = steggy.conceal()(original, message);
   let encoded64 = buffer.toString('base64');
-
-  // fs.writeFile("../../../../desktop/outGRRRRR.png", concealed, 'base64', function(err) {
-  //   if(err) console.log(err);
-  //   else console.log("fin!");
-  // });
 
   res.json({encodedImageData:encoded64})
 })
@@ -38,8 +30,7 @@ app.post('/ping', function (req, res) {
 //Encryption Services
 app.post('/encryption', function(req,res){
   var base64Data = req.body.imageData.replace(/^data:image\/png;base64,/, "");
-  console.log("\n\nbase64Data=>",base64Data,"\n\n");
-
+  
   require("fs").writeFile("./out.png", base64Data, 'base64', function(err) {
     if(err) console.log(err);
     else console.log("fin!");
@@ -48,6 +39,15 @@ app.post('/encryption', function(req,res){
   res.send(200);
 });
 
+
+app.post('/decrypt', function(req,res){
+  var base64Data = req.body.imageData.replace(/^data:image\/png;base64,/, "");
+  var original = new Buffer(base64Data, 'base64');
+  var buffer = steggy.reveal()(original);
+  var decryptedMessage = buffer.toString();
+
+  res.json({decryptedMessage:decryptedMessage});
+});
 
 
 
