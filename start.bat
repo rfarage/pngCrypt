e7ff762775c65b486187b8622611a2269c581d75
Zoid <brandincurry@gmail.com>
2018-10-10 17:49:58 -0400

    bat and shell scripts to start both services
    SHELL MAY OR MAY NOT WORK, im working on a windows machine

---
+++ start.bat
@@ -0,0 +1,5 @@
+cd app_service
+start cmd /k rails s
+cd ../
+cd crypt_service
+start cmd /k node app
---
+++ start.sh
@@ -0,0 +1,6 @@
+#this needs to be updated!
+cd app_service
+terminal -e --noclose rails s
+cd ../
+cd crypt_service
+terminal -e --noclose node app
