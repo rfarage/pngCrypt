27705132a0341a2c7825e31128f4b5b9d44930cd
Zoid <brandincurry@gmail.com>
2018-10-21 22:54:53 -0400

    file clean up, embeded wikipedia on about until we arent cba to write content

--- app_service/config/routes.rb
+++ app_service/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get     '/login',     to: 'sessions#new'
   post    '/login',     to: 'sessions#create'
   delete  '/logout',    to: 'sessions#destroy'
-  post    'users/:id',   to: 'users#reset'
+  post    '/users/:id',   to: 'users#reset'
   resources :users
 
   # Api
